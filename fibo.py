7ac33fead15a701b214399c35b948a4a002ca158
yasuppy <iyasunarii@gmail.com>
2025-06-28 19:06:44 +0900

    Add fibo.py script

---
+++ fibo.py
@@ -0,0 +1,10 @@
+# fibo.py
+def fibonacci_upto(limit):
+    a, b = 0, 1
+    while a <= limit:
+        print(a, end=' ')
+        a, b = b, a + b
+    print()
+
+if __name__ == "__main__":
+    fibonacci_upto(200)
